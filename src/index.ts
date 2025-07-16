fbb097c6599f745d5ebd258d072e515dc6fe5be0
Rahmat Ardiansyah <artsbymat@gmail.com>
2025-07-16 10:56:16 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ const app = new Elysia({ prefix: "/api" }).get(
   () => `Hello from bun@${Bun.version}`,
 );
 
+if (process.env.NODE_ENV !== "production") {
+  app.listen({ port: 3000 });
+  console.log("Server is running on http://localhost:3000");
+}
+
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
